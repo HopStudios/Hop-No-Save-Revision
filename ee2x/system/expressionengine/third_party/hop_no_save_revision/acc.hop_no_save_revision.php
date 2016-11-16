62bfc4fbcff3dcfbef17c141488f7f1c0d0c11ee
Louis DEKEISTER <louis@hopstudios.com>
2016-11-16 15:13:13 -0800

    add separate config file

--- ee2x/system/expressionengine/third_party/hop_no_save_revision/acc.hop_no_save_revision.php
+++ ee2x/system/expressionengine/third_party/hop_no_save_revision/acc.hop_no_save_revision.php
@@ -1,10 +1,12 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+require_once PATH_THIRD.'hop_no_save_revision/config.php';
+
 class Hop_no_save_revision_acc
 {
 	var $name			= 'Hop No Save Revision';
 	var $id				= 'hop_no_save_revision';
-	var $version		= '1.0';
+	var $version		= HOP_NO_SAVE_REVISION_VERSION;
 	var $description	= 'Remove the "Save revision" button in the publish screen';
 	var $sections		= array();
 
---
+++ ee2x/system/expressionengine/third_party/hop_no_save_revision/config.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('EXT')) exit('No direct script access allowed');
+
+/**
+ * Hop No Save Revision
+ *
+ * NSM Addon Updater config file.
+ *
+ * @package		Hop Studios:Hop No Save Revision
+ * @author		Hop Studios, Inc.
+ * @copyright	Copyright (c) 2016, Hop Studios, Inc.
+ * @link		http://www.hopstudios.com/software/
+ * @version		1.0
+ */
+
+$config['name']='Hop No Save Revision';
+$config['version']='1.0';
+$config['nsm_addon_updater']['versions_xml']='http://www.hopstudios.com/software/';
+
+// Version constant
+if (!defined("HOP_NO_SAVE_REVISION_VERSION")) {
+	define('HOP_NO_SAVE_REVISION_VERSION', $config['version']);
+}
